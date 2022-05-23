0296b4d988b698917ca792b4e3e4f60bdec5421a
thanasis <den14531@hotmail.com>
2022-05-23 17:42:52 +0300

    23/05/2022

--- js/tip.js
+++ js/tip.js
@@ -1,36 +1,36 @@
 // restip
 
-function tipValue(amt,np,pt)
-{
- return tip = (amt * pt) ; 	
- 	
-}
+//function tipValue(amt,np,pt)
+//{
+ //return tip = (amt * pt) ;
+
+//}
 function Calc()
 {
 	var amt = numericVal(document.calc.amt.value);
-	var pt = numericVal(document.calc.pt.value)/100;
+	//var pt = numericVal(document.calc.pt.value)/100;
 	var np   = numericVal(document.calc.np.value);
-	var tot = amt*(1+pt);
-	var sp = tot/np;
-		
-	if (amt==0 ||np==0)
-   { alert("NOTICE : \n Incomplete data \n Datos incompletos \n Dados incompletos "); 
-    
-	document.calc.tip.value = "";
-	document.calc.tot.value = "";
-	document.calc.sp.value = "";
-  }
-  else	
-  {	
-	
-	document.calc.tip.value = preFormat(k(amt,np,pt),2);
-	document.calc.tot.value = preFormat((amt*(1+pt)),2); 
-  document.calc.sp.value = preFormat((tot/np),2); 
-	  }	
-}
-function k(amt,np,pt)
+	//var tot = amt;
+	var sp = amt/np;
 
-{
-	return tipValue(amt,np,pt);
-	
+	//if (amt==0 ||np==0)
+   //{ alert("NOTICE : \n Incomplete data");
+
+	//document.calc.tip.value = "";
+	//document.calc.tot.value = "";
+	//document.calc.sp.value = "";
+  //}
+  //else
+  {
+
+	//document.calc.tip.value = preFormat(k(amt,np,pt),2);
+	//document.calc.tot.value = preFormat(amt,2);
+  document.calc.sp.value = preFormat((amt/np),2);
+	  }
 }
+//function k(amt,np)
+
+//{
+	//return tipValue(amt,np);
+
+//}
